f60cd094b44df48f998afe2397e1fa7c48410c7b
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2026-01-28 18:08:06 +0200

    initial commit

---
+++ example/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/alex/github-repos/opa-agency/onepage-portofolio-1/example/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/37e1d48fe87dd79a-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
